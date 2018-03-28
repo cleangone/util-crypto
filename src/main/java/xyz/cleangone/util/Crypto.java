061a4a39483c704def1b74eb4d769b7c1754fdfb
Andy Robbins <andy.robbins@comcast.com>
2018-03-28 16:58:48 -0700

    first commit

---
+++ src/main/java/xyz/cleangone/util/Crypto.java
@@ -0,0 +1,64 @@
+package xyz.cleangone.util;
+
+import org.apache.commons.codec.binary.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Crypto
+{
+    private static Crypto CRYPTO = null;
+
+    private IvParameterSpec ivParameterSpec;
+    private SecretKeySpec secretKeySpec;
+    private Cipher cipher;
+
+    public static String encrypt(String toBeEncrypt)
+    {
+        if (CRYPTO == null) { CRYPTO = new Crypto(); }
+
+        try
+        {
+            CRYPTO.cipher.init(Cipher.ENCRYPT_MODE, CRYPTO.secretKeySpec, CRYPTO.ivParameterSpec);
+            byte[] encrypted = CRYPTO.cipher.doFinal(toBeEncrypt.getBytes());
+            return Base64.encodeBase64String(encrypted);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Error encrypting", e);
+        }
+    }
+
+    public static String decrypt(String encryptedText)
+    {
+        if (CRYPTO == null) { CRYPTO = new Crypto(); }
+
+        try
+        {
+            CRYPTO.cipher.init(Cipher.DECRYPT_MODE, CRYPTO.secretKeySpec, CRYPTO.ivParameterSpec);
+
+            byte[] decoded = Base64.decodeBase64(encryptedText.getBytes());
+            byte[] decrypted = CRYPTO.cipher.doFinal(decoded);
+            return new String(decrypted);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Error decrypting", e);
+        }
+    }
+
+    private Crypto()
+    {
+        try
+        {
+            ivParameterSpec = new IvParameterSpec(CleangoneEnv.IV_PARAM_KEY.getBytes("UTF-8"));
+            secretKeySpec = new SecretKeySpec(CleangoneEnv.SECRET_KEY.getBytes("UTF-8"), "AES");
+            cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Error creating Crypto", e);
+        }
+    }
+
+}
